3e3d988bd63f287f58970c5c28c64ae52f214528
Ruang Romero <ruangromero@MacBook-Air-khxng-Ruang.local>
2026-05-29 20:04:59 +0700

    Move files to docs folder for GH Pages

---
+++ docs/js/main.js
@@ -0,0 +1,8 @@
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('rainbow-theme');
+});
+
+function setLanguage(lang) {
+    console.log('Language set to:', lang);
+    // Placeholder for future multi-language implementation
+}
